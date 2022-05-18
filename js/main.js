1a83f8e73e305f49ce5db5cc2f12abf1f31aa791
Dmitriy Coderoff <79995631+dcoderoff@users.noreply.github.com>
2022-05-18 18:34:33 +0000

    Use classes to create timers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-import {createTimer} from './timer.js';
+import {Timer} from './timer.js';
 
 const mainEl = document.querySelector('.main');
 const timerTemplate = document.querySelector('template#pomo');
 
-mainEl.append(createTimer(timerTemplate));
-mainEl.append(createTimer(timerTemplate));
+const t1 = new Timer(timerTemplate, {color: 'red'});
+const t2 = new Timer(timerTemplate, {renderInterval: 5000});
+
+t1.attach(mainEl);
+t2.attach(mainEl);
--- js/timer.js
+++ js/timer.js
@@ -1,35 +1,61 @@
-import {addZero} from './util.js';
+import {Component} from './component.js';
 
-const POMO_INTERVAL = 25 * 60 * 1000;
-const POMO_MIN = new Date(POMO_INTERVAL).getMinutes();
-const POMO_SEC = new Date(POMO_INTERVAL).getSeconds();
-
-export const createTimer = (template) => {
-  const timerEl = template.content.cloneNode(true);
+export class Timer extends Component {
+  static POMO_INTERVAL = 25 * 60 * 1000;
+  static POMO_MIN = '25';
+  static POMO_SEC = '00';
+  static DEFAULT_RENDER_INTERVAL = 100;
   
-  const pomoMinElem = timerEl.querySelector('.pomo__min');
-  const pomoSecElem = timerEl.querySelector('.pomo__sec');
-  const playBtn = timerEl.querySelector('.play__button');
+  static addZero(num) {
+    return `0${num}`.slice(-2);
+  }
 
-  let interval, lastStartTimestamp;
-  let state = 'paused';
-  let timeLeft = POMO_INTERVAL;
+  constructor(template, options = {}) { 
+    super();
+    this._el = template.content.querySelector('.pomo').cloneNode(true);
+    this._pomoMinElem = this._el.querySelector('.pomo__min');
+    this._pomoSecElem = this._el.querySelector('.pomo__sec');
+    this._playBtn = this._el.querySelector('.play__button');
+    this._timeLeft = Timer.POMO_INTERVAL;
+    this._lastStartTimestamp = undefined;
+    this._interval = undefined;
+    this._state = 'paused';
+    this._playBtn.addEventListener('click',       
+    this._handleChangeTimerState.bind(this));
+    this._renderInterval = options.renderInterval || Timer.DEFAULT_RENDER_INTERVAL;
+    this._el.style.color = options.color;
+  }
+
+  remove() {
+    super.remove();
+    this.log('Hooray! Timer is removed!');
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  log() {
+    console.log('Timer: ', ...arguments);
+    // method chaining
+    return this;
+  }
   
-  const renderTimer = () => {
-    const msPassedSinceStart = Date.now() - lastStartTimestamp;
-    const currentTimestamp = timeLeft - msPassedSinceStart;
+  _renderTimer() {
+    const msPassedSinceStart = Date.now() - this._lastStartTimestamp;
+    const currentTimestamp = this._timeLeft - msPassedSinceStart;
     const renderDate = new Date(currentTimestamp);
   
     if (currentTimestamp <= 0) {
-      pomoMinElem.textContent = `${POMO_MIN}`;
-      pomoSecElem.textContent = `${POMO_SEC}`;
+      this._pomoMinElem.textContent = `${Timer.POMO_MIN}`;
+      this._pomoSecElem.textContent = `${Timer.POMO_SEC}`;
       
-      playBtn.classList.toggle('play__button_state-active');
-      playBtn.classList.toggle('play__button_state-paused');
+      this._playBtn.classList.toggle('play__button_state-active');
+      this._playBtn.classList.toggle('play__button_state-paused');
   
-      state = 'paused';
-      timeLeft = POMO_INTERVAL;
-      clearInterval(interval);
+      this._state = 'paused';
+      this._timeLeft = Timer.POMO_INTERVAL;
+      clearInterval(this._interval);
       
       return;
     }
@@ -37,32 +63,28 @@ export const createTimer = (template) => {
     const minutes = renderDate.getMinutes();
     const seconds = renderDate.getSeconds();
     
-    pomoMinElem.textContent = addZero(minutes);
-    pomoSecElem.textContent = addZero(seconds);
-  };
-  
-  const handleChangeTimerState = () => {
-    switch (state) {
+    this._pomoMinElem.textContent = Timer.addZero(minutes);
+    this._pomoSecElem.textContent = Timer.addZero(seconds);
+  }
+
+  _handleChangeTimerState() {
+    switch (this._state) {
       case 'active':
-        state = 'paused';
-        const msPassedSinceStart = Date.now() - lastStartTimestamp;
-        timeLeft -= msPassedSinceStart;
-        clearInterval(interval);
+        this._state = 'paused';
+        const msPassedSinceStart = Date.now() - this._lastStartTimestamp;
+        this._timeLeft -= msPassedSinceStart;
+        clearInterval(this._interval);
         break;
       case 'paused':
-        lastStartTimestamp = Date.now();
-        state = 'active';
-        interval = setInterval(renderTimer, 100);
+        this._lastStartTimestamp = Date.now();
+        this._state = 'active';
+        this._interval = setInterval(this._renderTimer.bind(this), this._renderInterval);
         break;
       default:
         break;
     }
     
-    playBtn.classList.toggle('play__button_state-active');
-    playBtn.classList.toggle('play__button_state-paused');
-  };
-  
-  playBtn.addEventListener('click', handleChangeTimerState);
-
-  return timerEl;
-};
+    this._playBtn.classList.toggle('play__button_state-active');
+    this._playBtn.classList.toggle('play__button_state-paused');
+  }
+}
